554f65aec78eb090ae274128bee695befed2200e
lonewanderer27 <adrianejames27@gmail.com>
2023-12-19 23:17:29 +0800

    only display the events made by current admin user

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -7,10 +7,11 @@ use Illuminate\Http\Request;
 
 class StatisticsController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
 
         $eventsQuery = Event::query();
-        $events = $eventsQuery->withCount('attendees')->withCount('guests')->get();
+        $events = $eventsQuery->withCount('attendees')->withCount('guests')->where("user_id", auth()->id())->get();
 
         return view('statistics', [
             'events' => $events,
